ec5aee92d2eae9f43cae64c9ce374d8589fd4a7c
sguerlet <56584110+sguerlet@users.noreply.github.com>
2025-12-02 01:31:51 +0100

    Update site URL and add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import cloudflare from '@astrojs/cloudflare';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://visudrone.fr/",
   integrations: [
     tailwind(),
     icon(),
@@ -18,4 +19,5 @@ export default defineConfig({
       },
     }),
   ],
+  adapter: cloudflare()
 });
